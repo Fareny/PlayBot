415bed7e5b38c949f33aaa8a2f2a4dfb2eb5b70d
Alex Markov <pd.fareny@gmail.com>
2022-02-03 13:46:38 +0300

    Сделал рекурсию

--- playbot.js
+++ playbot.js
@@ -3,29 +3,32 @@ function one(x) {
     let result = Math.floor(Math.random() * (100 - 1)) + 1;
     function two() {
         count = +prompt('Угадай число от 1 до 100');
-        if (count == false) {
+        if (isNaN(count)) {
+            alert('Введи число!');
+        } if (count == false) {
             alert('Игра окончена!');
         } else if (x == 0) {
             alert("Попытки закончились, хотите сыграть еще?");
             one(9);
         }
-        while (x != 0) {
-            if (isNaN(count)) {
-                alert('Введи число!');
-            } else if (count < result && count != false) {
-                alert('Загаданное число больше, осталось попыток ' + x);
-                x--;
-            } else if (count > result && count != false) {
-                alert('Загаданное число меньше, осталось попыток ' + x);
-                x--;
-            } else if (count == result && count != false) {
-                alert('Поздравляю, Вы угадали!!! (Хотели бы сыграть еще?)');
-                one(9);
-            }
-            if (count !== result && count != false) {
-                two();
+        function three() {
+            if (x != 0) {
+                if (count < result && count != false) {
+                    alert('Загаданное число больше, осталось попыток ' + x);
+                    x--;
+                } else if (count > result && count != false) {
+                    alert('Загаданное число меньше, осталось попыток ' + x);
+                    x--;
+                } else if (count == result && count != false) {
+                    alert('Поздравляю, Вы угадали!!! (Хотели бы сыграть еще?)');
+                    one(9);
+                }
+                if (count !== result && count != false) {
+                    two();
+                }
             }
         }
+        three();
     }
     two();
 }
